7ca68b5f4b7036b40edb5df6e4386430aad52570
s22004 <s22004@std.it-college.ac.jp>
2024-08-07 16:12:37 +0900

    4a 追加

---
+++ 4a/4a-001c.php
@@ -0,0 +1,10 @@
+<?php
+  define('TMPLDIR', '/var/www/html/4a/tmpl/');
+  $tmpl = filter_input(INPUT_GET, 'template');
+  if (preg_match('/\A[a-z0-9]+\z/ui', $tmpl) !== 1) {
+    die('templateは英数字のみ指定でkます');
+?>
+<body>
+<?php readfile(TMPLDIR . $tmpl . '.html'); ?>
+メニュー（以下略）
+</body>
